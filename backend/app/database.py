0ee6b3baf4ad67a406caa160c477d9414890ac57
ayushi771 <ansomani1007@gmail.com>
2026-06-17 14:38:30 +0530

    Simplify DATABASE_URL initialization
    Refactor DATABASE_URL assignment to use a single line.

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,15 +4,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker, declarative_base
 
 from passlib.hash import bcrypt
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = (
-    f"postgresql+asyncpg://{os.getenv('POSTGRES_USER','postgres')}:"
-    f"{os.getenv('POSTGRES_PASSWORD','ayushi')}@"
-    f"{os.getenv('POSTGRES_HOST','localhost')}:"
-    f"{os.getenv('POSTGRES_PORT','5432')}/"
-    f"{os.getenv('POSTGRES_DB','herodb')}"
-)
-engine  = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
 
